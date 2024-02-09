dab351966092afc5ea2fc9bd7b4cf0db983bd66b
Todd H <github@simplybest.com>
2024-02-08 19:36:41 -0700

    integrate budi changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { dummyMessages, pickerIcon, sendIcon } from './app.const';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.scss'],
 })
-export class AppComponent { }
+export class AppComponent {
+  @ViewChild('messageInput') messageInput: ElementRef<HTMLTextAreaElement> | undefined = undefined;
+  message = '';
+  messages: string[] = [];
+  showPicker = false;
+  pickerIcon = pickerIcon;
+  sendIcon = sendIcon;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  sendMessage() {
+    if (this.message.trim()) {
+      console.log(this.message);
+      this.messages.push(this.message);
+      this.message = '';
+    }
+    this.messageInput?.nativeElement.focus();
+  }
+
+  keyDownEnter(event: any) {
+    event.preventDefault();
+    if (!event.shiftKey) {
+      this.sendMessage();
+    }
+  }
+
+  getSafeSvg(icon: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(icon);
+  }
+
+  ngOnInit() {
+    // Laden der Dummy-Nachrichten beim Initialisieren
+    this.messages = [...dummyMessages];
+  }
+}
---
+++ src/app/app.const.ts
@@ -0,0 +1,23 @@
+export const pickerIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path fill="#f3f3f3" d="M480-40q-149 0-254.5-42.5T120-200q0-32 20-57.5t56-45.5l65 58q-24 8-42.5 20.5T200-200q0 26 81 53t199 27q118 0 199-27t81-53q0-12-18.5-24.5T699-245l65-58q36 20 56 45.5t20 57.5q0 75-105.5 117.5T480-40Zm0-160q-22 0-42.5-7.5T400-230L148-453q-13-11-20.5-27t-7.5-33v-80q0-17 6.5-33t19.5-27l252-235q17-16 38-24t44-8q23 0 44 8t38 24l252 235q13 11 19.5 27t6.5 33v80q0 17-7.5 33T812-453L560-230q-17 15-37.5 22.5T480-200Zm-4-188q14 0 26.5-4.5T526-407l222-197-240-226q-7-5-14-7.5t-15-2.5q-8 0-15 2.5t-12 7.5L208-600l218 193q11 10 23.5 14.5T476-388ZM360-550q21 0 35.5-14.5T410-600q0-21-14.5-35.5T360-650q-21 0-35.5 14.5T310-600q0 21 14.5 35.5T360-550Zm50 54q43 21 90.5 13.5T584-522q34-29 44.5-73T618-678L410-496Zm70-174q21 0 35.5-14.5T530-720q0-21-14.5-35.5T480-770q-21 0-35.5 14.5T430-720q0 21 14.5 35.5T480-670Zm-2 56Z"/></svg>';
+
+export const sendIcon =
+  '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path fill="#f3f3f3" d="M792-443 176-183q-20 8-38-3.5T120-220v-520q0-22 18-33.5t38-3.5l616 260q25 11 25 37t-25 37ZM200-280l474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"/></svg>';
+
+export const dummyMessages = [
+  'Hey Team, I just started working on the new feature.',
+  'So, I used a CSS framework, and guess what? It broke everything!',
+  'Turns out, I forgot to close a div. Classic frontend developer mistake.',
+  'Fixed it, but now the header is all wonky.',
+  'Never mind, it was just a cache issue. Have you tried turning it off and on again?',
+  "Okay, who added a 'z-index' of 9999 to the modal overlay?",
+  'Sigh, the client wants us to make the logo bigger again.',
+  "Just realized I've been editing the wrong file for the past hour...",
+  'Good news! The page finally loads in under 3 seconds.',
+  "Bad news... it's completely blank on Internet Explorer.",
+  'Why do we still support IE, again?',
+  "Update: I've added a new feature. It's a spinning loader that never stops. It's mesmerizing.",
+  "Remember the spinning loader? It's now the company's favorite feature. Go figure!",
+  "I've started dreaming in JavaScript. Is that normal?",
+  "Last one for today: I fixed a bug, and nothing else broke. I'm just as surprised as you are!",
+];
